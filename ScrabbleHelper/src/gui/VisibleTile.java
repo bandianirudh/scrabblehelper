72e1eb47cc0ce79b784e4ec57913e1e389554154
nwilkie <nwilkie@e150fe50-cd3d-0410-8577-2ffe21290fbb>
2008-01-12 01:18:07 +0000

    Cleaned it up a bit.

--- ScrabbleHelper/src/gui/VisibleTile.java
+++ ScrabbleHelper/src/gui/VisibleTile.java
@@ -17,11 +17,9 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
 import javax.swing.AbstractAction;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
 import scrabbletools.BoardLayout;
@@ -97,68 +95,6 @@ public class VisibleTile extends JPanel implements MouseListener, FocusListener,
         if (hasFocus()) {
             System.out.println("hasFocus");
         }
-
-        /*
-        editor.setDocument(new PlainDocument() {
-        public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-        if (Arrays.binarySearch(LetterScores.allLetters, str.charAt(0)) >= 0) {// || str.charAt(0) == LetterScores.EMPTY_SQUARE) {
-        super.remove(0, super.getLength());
-        if (Character.isUpperCase(str.charAt(0))) {
-        super.insertString(0, str.toLowerCase(), a);
-        } else {
-        super.insertString(0, str.toUpperCase(), a);
-        }
-        if (editor.getText().length() > 0) {
-        editor.setSelectionStart(0);
-        editor.setSelectionEnd(1);
-        }
-        if (isEditing() &&
-        Character.toUpperCase(str.charAt(0)) != Character.toUpperCase(getLetter())) {
-        panel.letterEnterred(VisibleTile.this, editor);
-        }
-        }
-        }; 
-        public void removeUpdate(DefaultDocumentEvent chng) {
-        VisibleTile.this.repaint();
-        super.removeUpdate(chng);
-        }
-        });
-        registerKeys(editor);
-        editor.setFont(Font.decode("Arial 22"));
-        if (Character.isUpperCase(getLetter())) {
-        editor.setText(Character.toString(getLetter()).toLowerCase());
-        //System.out.println("Upper");
-        } else {
-        editor.setText(Character.toString(getLetter()).toUpperCase());
-        //System.out.println("Lower");
-        }
-        if (editor.getText().length() > 0) {
-        editor.setSelectionStart(0);
-        editor.setSelectionEnd(1);
-        }
-        editor.addFocusListener(new FocusListener() {
-        public void focusGained(FocusEvent e) {
-        }
-        public void focusLost(FocusEvent e) {
-        if (editor.getText().length() > 0) {
-        setLetter(editor.getText().charAt(0));
-        }
-        stopEditing(editor);
-        }
-        });
-        editor.setHorizontalAlignment(SwingConstants.CENTER);
-        editor.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-        stopEditing(editor);
-        }
-        });
-        //editor.setForeground(new Color(125, 125, 125, 200));
-        //editor.setForeground(new Color(125, 125, 125, 200));
-        remove(letterLabel);
-        add(editor, BorderLayout.CENTER);
-        setEditing(true);
-        editor.requestFocus();
-         */
         repaint();
         doLayout();
         Rectangle r = getBounds();
@@ -166,19 +102,13 @@ public class VisibleTile extends JPanel implements MouseListener, FocusListener,
     }
 
     public void paintComponent(Graphics g) {
-        /*
-        if (getComponentCount() > 0) {
-        super.paintComponent(g);
-        }
-         */
+        //First draw the background color
         g.setColor(BoardLayout.getColorFromTileType(tileType));
         g.fillRect(1, 1, getWidth(), getHeight());
         if (isEditing()) {
+            //Draw the box for editing
             g.setColor(Color.WHITE);
             g.fillRect(1, 1, getWidth(), getHeight());
-            letterLabel.paint(g);
-            //g.setColor(BoardLayout.getColorFromTileType(tileType));
-            //g.fillRect(1, 1, getWidth(), getHeight());
             g.setColor(new Color(100, 100, 255, 200));
             g.fillRect(1, 1, getWidth(), getHeight());
 
@@ -191,8 +121,9 @@ public class VisibleTile extends JPanel implements MouseListener, FocusListener,
                 g.drawLine(getWidth() / 2, 1, getWidth() / 2, lineLength);
                 g.drawLine(getWidth() / 2, getHeight() - lineLength, getWidth() / 2, getHeight());
             }
-        } else if (letter != LetterScores.EMPTY_SQUARE) {
             letterLabel.paint(g);
+        } else if (letter != LetterScores.EMPTY_SQUARE) {
+            //If there is a tile here, draw it like a tile.
             Color emptyColor = BoardLayout.getColorFromTileType(BoardLayout.SINGLE_LETTER);
             g.setColor(new Color(emptyColor.getRed(), emptyColor.getGreen(), emptyColor.getBlue(), 160));
             g.fillRect(1, 1, getWidth() - 1, getHeight() - 1);
@@ -202,50 +133,23 @@ public class VisibleTile extends JPanel implements MouseListener, FocusListener,
             g.fillRect(3, 3, getWidth() - 5, getHeight() - 5);
             g.setColor(new Color(255, 255, 255, 90));
             g.fillRect(4, 4, getWidth() - 7, getHeight() - 7);
+            letterLabel.paint(g);
         }
     }
 
-    public void paint(Graphics g) {
-        super.paint(g);
-    /*
-    if (isEditing() && getComponent(0) != null && getComponent(0) instanceof JTextField) {
-    g.setColor(Color.WHITE);
-    g.fillRect(1, 1, getWidth(), getHeight());
-    letterLabel.paint(g);
-    //g.setColor(BoardLayout.getColorFromTileType(tileType));
-    //g.fillRect(1, 1, getWidth(), getHeight());
-    g.setColor(new Color(100, 100, 255, 200));
-    g.fillRect(1, 1, getWidth(), getHeight());
-    g.setColor(Color.BLACK);
-    int lineLength = 5;
-    if (panel.isMoveAcross()) {
-    g.drawLine(1, getHeight() / 2, 5, getHeight() / 2);
-    g.drawLine(getWidth() - lineLength, getHeight() / 2, getWidth(), getHeight() / 2);
-    } else {
-    g.drawLine(getWidth() / 2, 1, getWidth() / 2, lineLength);
-    g.drawLine(getWidth() / 2, getHeight() - lineLength, getWidth() / 2, getHeight());
-    }
-    } else if (letter != LetterScores.EMPTY_SQUARE) {
-    super.paint(g);
-    } else {
-    super.paint(g);
-    }
-     */
-    }
-
     public void mouseClicked(MouseEvent e) {
-    }
-
-    public void mousePressed(MouseEvent e) {
-    }
-
-    public void mouseReleased(MouseEvent e) {
         Rectangle r = getBounds();
         r.setLocation(0, 0);
         if (r.contains(e.getPoint())) {
             edit();
             requestFocus();
         }
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
     }
 
     public void mouseEntered(MouseEvent e) {
@@ -301,7 +205,6 @@ public class VisibleTile extends JPanel implements MouseListener, FocusListener,
 
             public void actionPerformed(ActionEvent e) {
                 VisibleTile.this.setLetter(LetterScores.EMPTY_SQUARE);
-                //stopEditing(editor);
                 sbp.letterDeleted(VisibleTile.this);
             }
         });
